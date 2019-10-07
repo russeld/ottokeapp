c0d1a5b8cdb0fd2f311d2b4bb70fcfd7db663266
Russel Baoy <russel@prideandjoy.org>
2019-10-07 21:59:57 +1300

    adding pwa functionalities

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,30 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+
+mix.webpackConfig({
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'pwa',
+      filename: 'service-worker.js',
+      staticeFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+      minify: true,
+      stripPrefix: 'public/',
+      handleFetch: true,
+      dynamicUrlToDependencies: {
+        '/': ['resources/views/home.blade.php']
+      },
+      staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+      navigateFallback: '/',
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+          handler: 'cacheFirst'
+        }
+      ]
+    })
+  ]
+})
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
